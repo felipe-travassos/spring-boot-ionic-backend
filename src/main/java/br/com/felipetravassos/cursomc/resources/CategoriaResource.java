8c4d287c9febda48cfc1971231a6d3e612f63bda
Felipe <ftravassos.icm@hotmail.com>
2018-09-15 22:48:43 -0300

    Testando a primeira classe de domínio

--- src/main/java/br/com/felipetravassos/cursomc/resources/CategoriaResource.java
+++ src/main/java/br/com/felipetravassos/cursomc/resources/CategoriaResource.java
@@ -1,16 +1,38 @@
 package br.com.felipetravassos.cursomc.resources;
+//Resources é um nome padrão que grava as controladoras REST.
+
+import java.util.ArrayList;
+import java.util.List;
+
+//Representational State Transfer, abreviado como REST, 
+//não é uma tecnologia, uma biblioteca, e nem tampouco 
+//uma arquitetura, mas sim um modelo a ser utilizado para 
+//se projetar arquiteturas de software distribuído, baseadas 
+//em comunicação via rede.
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-@RequestMapping(value="/categorias")
+import br.com.felipetravassos.cursomc.domain.Categoria;
+
+@RestController // Diretiva Controlador REST que vai responder pelo End Point abaixo.
+@RequestMapping(value = "/categorias") // End point REST
 public class CategoriaResource {
 
-	@RequestMapping(method=RequestMethod.GET)
-	public String listar() {
-		return "REST está funcionando!";
+	@RequestMapping(method = RequestMethod.GET)
+	public List<Categoria> listar() {
+
+		Categoria cat1 = new Categoria(1, "Informática");
+		Categoria cat2 = new Categoria(2, "Escritorio");
+
+		List<Categoria> lista = new ArrayList<>();// List é uma interface e
+													// não pode ser importado
+													// ArrayList implementa uma lista.
+		lista.add(cat1);
+		lista.add(cat2);
+
+		return lista;
 	}
-	
+
 }
