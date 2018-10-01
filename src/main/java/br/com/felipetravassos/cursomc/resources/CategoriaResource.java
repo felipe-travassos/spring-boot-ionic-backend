f951a61c71c29f1bb51f5f3b69b96a30ffbe9ed0
Felipe <ftravassos.icm@hotmail.com>
2018-10-01 12:32:41 -0300

    Listando todas as categorias com o DTO

--- src/main/java/br/com/felipetravassos/cursomc/resources/CategoriaResource.java
+++ src/main/java/br/com/felipetravassos/cursomc/resources/CategoriaResource.java
@@ -2,6 +2,8 @@ package br.com.felipetravassos.cursomc.resources;
 //Resources é um nome padrão que grava as controladoras REST.
 
 import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -13,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import br.com.felipetravassos.cursomc.domain.Categoria;
+import br.com.felipetravassos.cursomc.dto.CategoriaDTO;
 import br.com.felipetravassos.cursomc.services.CategoriaService;
 
 //Representational State Transfer, abreviado como REST, 
@@ -63,4 +66,15 @@ public class CategoriaResource {
 		service.delete(id);
 		return ResponseEntity.noContent().build();
 	}
+
+	@RequestMapping(method = RequestMethod.GET)
+	public ResponseEntity<List<CategoriaDTO>> findAll() {
+		List<Categoria> list = service.findAll();
+		// Pecorrer a lista pelo Stream() COllectors serve para converter uma lista para
+		// outra.
+		List<CategoriaDTO> listDto = list.stream().map(obj -> new CategoriaDTO(obj)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDto);
+
+	}
+
 }
