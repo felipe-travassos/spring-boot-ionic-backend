f668b53b1e59d57f86a08413498230adf11e4102
Felipe <ftravassos.icm@hotmail.com>
2018-10-18 11:17:48 -0300

    Inserindo Pedido

--- src/main/java/br/com/felipetravassos/cursomc/resources/PedidoResource.java
+++ src/main/java/br/com/felipetravassos/cursomc/resources/PedidoResource.java
@@ -1,9 +1,14 @@
 package br.com.felipetravassos.cursomc.resources;
 //Resources é um nome padrão que grava as controladoras REST.
 
+import java.net.URI;
+
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 
 //Representational State Transfer, abreviado como REST, 
 //não é uma tecnologia, uma biblioteca, e nem tampouco 
@@ -14,6 +19,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import br.com.felipetravassos.cursomc.domain.Pedido;
 import br.com.felipetravassos.cursomc.services.PedidoService;
@@ -32,6 +38,13 @@ public class PedidoResource {
 
 		Pedido obj = service.find(id);
 		return ResponseEntity.ok().body(obj);
+	}
+
+	@RequestMapping(method = RequestMethod.POST)
+	public ResponseEntity<Void> insert(@Valid @RequestBody Pedido obj) {
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/id").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
 	}
 
 }
--- src/main/java/br/com/felipetravassos/cursomc/services/PedidoService.java
+++ src/main/java/br/com/felipetravassos/cursomc/services/PedidoService.java
@@ -1,10 +1,19 @@
 package br.com.felipetravassos.cursomc.services;
 
+import java.util.Date;
 import java.util.Optional;
+
+import javax.transaction.Transactional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import br.com.felipetravassos.cursomc.domain.ItemPedido;
+import br.com.felipetravassos.cursomc.domain.PagamentoComBoleto;
 import br.com.felipetravassos.cursomc.domain.Pedido;
+import br.com.felipetravassos.cursomc.domain.enums.EstadoPagamento;
+import br.com.felipetravassos.cursomc.repositories.ItemPedidoRepository;
+import br.com.felipetravassos.cursomc.repositories.PagamentoRepository;
 import br.com.felipetravassos.cursomc.repositories.PedidoRepository;
 import br.com.felipetravassos.cursomc.services.exceptions.ObjectNotFoundException;
 
@@ -14,10 +23,43 @@ public class PedidoService {
 	@Autowired
 	private PedidoRepository repo;
 
+	@Autowired
+	private BoletoService boletoService;
+
+	@Autowired
+	private PagamentoRepository pagamentoRepository;
+
+	@Autowired
+	private ItemPedidoRepository itemPedidoRepository;
+
+	@Autowired
+	private ProdutoService produtoService;
+
 	// Metodo buscar o id da categoria no metodo GET
 	public Pedido find(Integer id) {
 		Optional<Pedido> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				"Objeto não encontrado! Id: " + id + ", Tipo: " + Pedido.class.getName()));
+	}
+
+	@Transactional
+	public Pedido insert(Pedido obj) {
+		obj.setId(null);
+		obj.setInstante(new Date());
+		obj.getPagamento().setEstado(EstadoPagamento.PENDENTE);
+		obj.getPagamento().setPedido(obj);
+		if (obj.getPagamento() instanceof PagamentoComBoleto) {
+			PagamentoComBoleto pagto = (PagamentoComBoleto) obj.getPagamento();
+			boletoService.preencherPagamentoComBoleto(pagto, obj.getInstante());
+		}
+		obj = repo.save(obj);
+		pagamentoRepository.save(obj.getPagamento());
+		for (ItemPedido ip : obj.getItens()) {
+			ip.setDesconto(0.0);
+			ip.setPreco(produtoService.find(ip.getProduto().getId()).getPreco());
+			ip.setPedido(obj);
+		}
+		itemPedidoRepository.saveAll(obj.getItens());
+		return obj;
 	}
 }
